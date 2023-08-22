b0c0929f324173c176b0bb5eb3f9e18e19d27e35
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2023-08-22 05:38:15 -0700

    add color red to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {},
     colors : {
-      "primary" : "#8593FF",
+      "primary" : "#D12620",
       "white": "#ffffff",
       "black":"#000000",
     },
